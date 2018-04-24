0a04ed9f9ba4774178e867b3dd45f80bf6292d37
Ewan <vulcanv6@hotmail.com>
2018-04-24 19:40:14 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/ewanpatrick/quicklauncher/MainActivity.java
@@ -0,0 +1,44 @@
+package uk.co.ewanpatrick.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Launch a different activity within app
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("uk.co.ewanpatrick.quick;launcher.SOMETHING", "¯\\_(ツ)_/¯ Oaky Dokey");
+                startActivity(startIntent);
+            }
+        });
+
+        // Attempt to launch a different app/ activity outside of the app
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "https://uk.ask.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+
+            }
+        });
+    }
+}
